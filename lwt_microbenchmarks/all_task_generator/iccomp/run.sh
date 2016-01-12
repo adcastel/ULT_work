2ba1c6792f1f6bd852aa1fb6b80ac2ac1bd84c7c
Adrian Castello <adcastel@uji.es>
2016-01-12 13:55:11 +0000

    ICC all producer test done

---
+++ lwt_microbenchmarks/all_task_generator/iccomp/run.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+
+
+export OMP_WAIT_POLICY=passive
+for i in {1..36}; do  OMP_NUM_THREADS=$i ./omp_task 100 1 >> icc_passive_all_task_100.dat; done
+for i in {1..36}; do  OMP_NUM_THREADS=$i ./omp_task 1000 1 >> icc_passive_all_task_1000.dat; done
+for i in {1..36}; do  OMP_NUM_THREADS=$i ./omp_task 2000 1 >> icc_passive_all_task_2000.dat; done
+for i in {1..36}; do  OMP_NUM_THREADS=$i ./omp_task 100 500 >> icc_passive_all_bigtask_100.dat; done
+for i in {1..36}; do  OMP_NUM_THREADS=$i ./omp_task 1000 500 >> icc_passive_all_bigtask_1000.dat; done
+for i in {1..36}; do  OMP_NUM_THREADS=$i ./omp_task 2000 500 >> icc_passive_all_bigtask_2000.dat; done
+
+
